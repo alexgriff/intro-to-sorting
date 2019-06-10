f7b91f38d9686912b0ecb2889a96f882ad737ab0
Alex Griffith <alexalexgriffith@gmail.com>
2019-06-10 11:58:44 -0400

    Update randomArray.js

--- randomArray.js
+++ randomArray.js
@@ -3,4 +3,4 @@ const randomArray = (length, max) =>
 
 const arr = randomArray(1000, 1000);
 const smallArr = randomArray(10, 100);
-const sortedArr = randomArray(2000, 1000).sort((a, b) => a - b);
+const sortedArr = randomArray(1000, 1000).sort((a, b) => a - b);
