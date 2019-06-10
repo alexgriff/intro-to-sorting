febb3b64b721a1100fdfa1e4ced32d54911d4111
Alex Griffith <alexalexgriffith@gmail.com>
2019-06-10 17:58:21 -0400

    fixes

--- binarysearch.js
+++ binarysearch.js
@@ -1,5 +1,5 @@
 // Binary Search
-// Q: What's the runtime? A: O(logN)
+// Q: What's the runtime?
 
 const binarySearch = (arr, target) => {
   console.log(`Remaining elements to search: ${arr.length}`);
@@ -7,23 +7,24 @@ const binarySearch = (arr, target) => {
   const midpoint = Math.floor(arr.length / 2);
   const middle = arr[midpoint];
 
-  const found = middle === target ? target : false;
+  // if (arr.length <= 1) {
+  //   return middle === target ? target : false;
+  // }
 
-  if (arr.length <= 1 || found) {
-    return found;
-  } else {
-    if (middle >= target) {
-      // search first half
-      return binarySearch(arr.slice(0, midpoint), target);
-    } else {
-      // search second half
-      return binarySearch(arr.slice(midpoint), target);
-    }
+  if (middle === target) {
+    // we found the thing
+    return target
+  } else if (middle >= target) {
+    // let's look in the first half
+    return binarySearch(arr.slice(0, midpoint), target);
+  } else if (middle < target){
+    // let's look in the second half
+    return binarySearch(arr.slice(midpoint), target);
   }
 };
 
 // Linear Search
-// Q: What's the runtime? A: O(n)
+// Q: What's the runtime?
 
 const linearSearch = (arr, target) => {
   for (let i = 0; i < arr.length; i++) {
